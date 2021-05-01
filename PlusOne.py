1e36a798ddf8738ab9c7e462a78e166944fe3aa1
eriysd <74118903+eriysd@users.noreply.github.com>
2021-05-01 13:55:56 -0400

    daily practice

---
+++ PlusOne.py
@@ -0,0 +1,25 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        '''
+        length = len(digits)
+        element = digits.pop() + 1
+        if element//10 > 1:
+            digits.append(digits.pop()+1)
+            digits.append(element%10)
+        else:
+            digits.append(element)
+        return digits
+        '''
+        
+        #solution
+        index = len(digits) -1
+        while index >= 0:
+            if digits[index] is not 9:
+                digits[index] += 1
+                return digits
+            else:
+                digits[index] = 0
+                index -= 1
+        #when every term was 9
+        return [1] + digits 
+    
