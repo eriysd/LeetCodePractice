f154e64b68147a3ecfe3137b89f41d1c3c86c77e
eriysd <74118903+eriysd@users.noreply.github.com>
2021-05-02 12:04:32 -0400

    daily practice

---
+++ SingleNumber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dict = {}
+        for num in nums:
+                dict[num] = dict.get(num, 0) + 1
+        for key, value in dict.items():
+            if value == 1:
+                return key
